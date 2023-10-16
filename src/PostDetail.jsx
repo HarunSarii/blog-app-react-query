1cfb01c81cb734e01393f1617f6fc9ee4c41ea82
HarunSarii <harunsari14@gmail.com>
2023-10-16 16:14:15 +0300

    feat: add pagination

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,8 +24,16 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["comment", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  console.log("commenthere", data);
+
+  if (isLoading) return <h3>Loading...</h3>;
+  if (error)
+    return <h3>Upss, sth went wrong. error: {JSON.stringify(error)}</h3>;
 
   return (
     <>
@@ -31,7 +41,7 @@ export function PostDetail({ post }) {
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {(data ?? []).map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
--- src/Posts.jsx
+++ src/Posts.jsx
@@ -5,24 +5,28 @@ import { useQuery } from "react-query";
 
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data, isError, isLoading } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
-  });
+  const { data, isError, error, isLoading } = useQuery(
+    {
+      queryKey: ["posts", currentPage],
+      currentPage,
+      queryFn: () => fetchPosts(currentPage),
+    },
+    { staleTime: 2000 }
+  );
 
   if (isLoading) return <h3>Loading...</h3>;
-  if (isError) return <h1>Ops, sth went wrong</h1>;
+  if (isError) return <h1>Ops, sth went wrong: {error.toString()}</h1>;
 
   return (
     <>
@@ -38,11 +42,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
